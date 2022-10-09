1bee974a557751b9c6f563b9943b3f15639987c8
thuong.nv <thuong.nv@gmail.com>
2022-10-09 23:43:00 +0700

    thread = draw + process + main

--- winsys_support/include/wglhandle.h
+++ winsys_support/include/wglhandle.h
@@ -48,11 +48,159 @@
 
 #define GL_WIN_CLASS  L"FOX_WINHANDLE_CLASS"
 
+#define   GL_PRESSED    1
+#define   GL_RELEASE    0
+
 //Không sử dụng Graphics vì nó bị đỏ khi Bug khó chịu @^@
 typedef Gdiplus::Graphics                   WndGDIplus;
 
+enum GLKeyboard
+{
+    AltLeft         = 0x12,      // (18)
+    AltRight        = 0x12,      // (18)
+    CapsLock        = 0x14,      // (20)
+    ControlLeft     = 0x11,      // (17)
+    ControlRight    = 0x11,      // (17)
+    OSLeft          = 0x5B,      // (91)
+    OSRight         = 0x5C,      // (92)
+    ShiftLeft       = 0x10,      // (16)
+    ShiftRight      = 0x10,      // (16)
+    Delete          = 0x2E,      // (46)
+    Escapex         = 0x1B,      // (27)
+
+    Digit1          = 0x31,      // (49)
+    Digit2          = 0x32,      // (50)
+    Digit3          = 0x33,      // (51)
+    Digit4          = 0x34,      // (52)
+    Digit5          = 0x35,      // (53)
+    Digit6          = 0x36,      // (54)
+    Digit7          = 0x37,      // (55)
+    Digit8          = 0x38,      // (56)
+    Digit9          = 0x39,      // (57)
+    Digit0          = 0x30,      // (48)
+    KeyA            = 0x41,      // (65)
+    KeyB            = 0x42,      // (66)
+    KeyC            = 0x43,      // (67)
+    KeyD            = 0x44,      // (68)
+    KeyE            = 0x45,      // (69)
+    KeyF            = 0x46,      // (70)
+    KeyG            = 0x47,      // (71)
+    KeyH            = 0x48,      // (72)
+    KeyI            = 0x49,      // (73)
+    KeyJ            = 0x4A,      // (74)
+    KeyK            = 0x4B,      // (75)
+    KeyL            = 0x4C,      // (76)
+    KeyM            = 0x4D,      // (77)
+    KeyN            = 0x4E,      // (78)
+    KeyO            = 0x4F,      // (79)
+    KeyP            = 0x50,      // (80)
+    KeyQ            = 0x51,      // (81)
+    KeyR            = 0x52,      // (82)
+    KeyS            = 0x53,      // (83)
+    KeyT            = 0x54,      // (84)
+    KeyU            = 0x55,      // (85)
+    KeyV            = 0x56,      // (86)
+    KeyW            = 0x57,      // (87)
+    KeyX            = 0x58,      // (88)
+    KeyY            = 0x59,      // (89)
+    KeyZ            = 0x5A,      // (90)
 
 
+    Left            = 0x25,      // (37)   Left arrow.
+    Up              = 0x26,      // (38)   Up arrow.
+    Right           = 0x27,      // (39)   Right arrow.
+    Down            = 0x28,      // (40)   Down arrow.
+
+
+    F1              = 0x70,      // (112)
+    F2              = 0x71,      // (113)
+    F3              = 0x72,      // (114)
+    F4              = 0x73,      // (115)
+    F5              = 0x74,      // (116)
+    F6              = 0x75,      // (117)
+    F7              = 0x76,      // (118)
+    F8              = 0x77,      // (119)
+    F9              = 0x78,      // (120)
+    F10             = 0x79,      // (121)
+    F11             = 0x7A,      // (122)
+    F12             = 0x7B,      // (123)
+    F13             = 0x7C,      // (124)
+    F14             = 0x7D,      // (125)
+    F15             = 0x7E,      // (126)
+    F16             = 0x7F,      // (127)
+    F17             = 0x80,      // (128)
+    F18             = 0x81,      // (129)
+    F19             = 0x82,      // (130)
+    F20             = 0x83,      // (131)
+    F21             = 0x84,      // (132)
+    F22             = 0x85,      // (133)
+    F23             = 0x86,      // (134)
+    F24             = 0x87,      // (135)
+};
+
+enum GLMouse
+{
+    LeftButton      = 0x01,      // (1)
+    RightButton     = 0x02,      // (2)
+    MidButton       = 0x04,      // (4)
+};
+
+class Safe_Thread
+{
+private:
+	std::thread		m_thread;
+	std::string		m_thread_name;
+
+	bool			m_bcreated;
+public:
+	
+	explicit Safe_Thread() :
+		m_thread(),
+		m_thread_name("none"),
+		m_bcreated(false)
+	{
+
+	}
+
+	template<class ...Args>
+	void create(Args &&...args)
+	{
+		m_thread =  std::move(std::thread(std::forward<Args>(args)...));
+		m_bcreated = true;
+	}
+
+	Safe_Thread(Safe_Thread &&other)
+	{
+		m_thread = std::move(other.m_thread);
+	}
+
+	~Safe_Thread()
+	{
+		join();
+	}
+
+	bool is_created()
+	{
+		return m_bcreated;
+	}
+
+	void setname(const std::string& name)
+	{
+		m_thread_name = name;
+	}
+
+	void join()
+	{
+		if (m_thread.joinable())
+			m_thread.join();
+	}
+
+	void detach()
+	{
+		if (m_thread.joinable())
+			m_thread.detach();
+	}
+};
 
 
 enum class FontWeight
@@ -169,6 +317,9 @@ private:
 	unsigned int           m_fontSizeTextRender;
 	bool                   m_bSysInfo;
 
+
+	Safe_Thread			   m_drawthread;
+	Safe_Thread			   m_processthread;
 
 	// Font information
 	const char*            m_fontName;
@@ -390,6 +541,8 @@ public:
 private:
 	virtual void OnDraw()
 	{
+		if (!this->MakeContext()) return;
+
 		this->UpdateWndInfo();
 
 		// Thực hiện vẽ custom người dùng
@@ -404,11 +557,10 @@ private:
 		{
 			char strsys[80];
 			sprintf_s(strsys, "fps: %d - frametime : %f ms", GetFPS(), GetFrameTime());
-			m_text_render.Use();
-			m_text_render.Write(10, 20, strsys);
-
-			m_text_render.DontUse();
+			this->WriteText(strsys, 10, 20);
 		}
+
+		this->SwapBuffer();
 	}
 
 private:
@@ -837,7 +989,7 @@ private:
 			this->UpdateHint();
 
 			// Create OpenGL context
-			this->CreateOpenGLContext(bInitOpenGLEx);
+			//this->CreateOpenGLContext(bInitOpenGLEx);
 
 			this->OnCreated();
 
@@ -908,6 +1060,7 @@ private:
 
 		// Setup Text render;
 		this->ReloadTextRender();
+
 	}
 
 	//===================================================================================
@@ -1136,7 +1289,7 @@ public:
 				return true;
 			}
 		}
-		assert(0);
+		//assert(0);
 		return false;
 	}
 
@@ -1262,15 +1415,44 @@ public:
 		DestroyControl();       //N.V.Thuong 22.04.2022
 	}
 
+private:
+
+	virtual void ThreadDrawOpenGL()
+	{
+		this->CreateOpenGLContext(true);
+
+		while (!this->closed())
+		{
+			this->OnDraw();
+		}
+	}
+
+	virtual void ThreadProcessOpenGL()
+	{
+		while (!this->closed())
+		{
+			this->OnProcess();
+		}
+	}
+
 public:
 
 	// function draw opengl : makecontext + swapbuffer
 	void draw()
 	{
-		if (this->MakeContext())
+		if (!m_drawthread.is_created())
 		{
-			this->OnDraw();
-			this->SwapBuffer();
+			m_drawthread.create(&Window::ThreadDrawOpenGL, this);
+			m_drawthread.detach();
+		}
+	}
+
+	void process()
+	{
+		if (!m_processthread.is_created())
+		{
+			m_processthread.create(&Window::ThreadProcessOpenGL, this);
+			m_processthread.detach();
 		}
 	}
 
@@ -1289,7 +1471,7 @@ public:
 	void wait_event()
 	{
 		// Xử lý trong trường hợp vẽ theo sự kiện
-		while (GetMessage(&m_msg, NULL, 0, 0) > 0)
+		if (GetMessage(&m_msg, NULL, 0, 0) > 0)
 		{
 			TranslateMessage(&m_msg);
 			DispatchMessage(&m_msg);
--- winsys_support/main.cpp
+++ winsys_support/main.cpp
@@ -51,6 +51,37 @@ void process_output()
     }
 }
 
+float x = 0.5;
+float a = -1.0;
+
+void MouseButton(Window* win)
+{
+	if (win->GetMouseButtonStatus(GLMouse::LeftButton))
+	{
+		if (a == -1.0)
+		{
+			a = 1.0;
+		}
+		else
+		{
+			a = -1.0;
+		}
+	}
+}
+
+void Process(Window* win)
+{
+	//for (int i = 0; i < 12; i++)
+	//{
+	//	for (int j = 0; j < 100; j++)
+	//	{
+	//		x += 0.001;
+	//		std::this_thread::sleep_for(10ms);
+	//	}
+	//}
+}
+
+
 void Draw(Window* win)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -58,22 +89,27 @@ void Draw(Window* win)
 	glColor3f(1.0, 0.0, 0.0);
 	glBegin(GL_LINES);
 	{
-		glVertex2f(0.5, 0.5);
-		glVertex2f(-0.5, 0.5);
+		glVertex2f(-x, a);
+		glVertex2f( x, a);
 	}
 	glEnd();
+	//a += 0.5* win->GetFrameTime();
 }
 
 int main()
 {
 	Window* win = fox_create_window(L"thường", 0, 0);
 	win->SetOnDrawfunc(Draw);
+	win->SetProcessfunc(Process);
+	win->SetOnMouseButtonfunc(MouseButton);
+
 	win->WriteSystemInfo(true);
 
 	if (win)
 	{
 		while (!win->closed())
 		{
+			win->process();
 			win->draw();
 			win->poll_event();
 		}
